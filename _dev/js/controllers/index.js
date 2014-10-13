21f50b707c8f25eaa5edb57d7742a549d695ca69
Lucas Mouilleron <lucas.mouilleron@me.com>
2014-10-14 01:27:33 +0200

    next2

--- _dev/js/controllers/index.js
+++ _dev/js/controllers/index.js
@@ -1,6 +1,6 @@
 /////////////////////////////////////////////////////////////////////
 // Controllers index (shortcut for future inclusions)
 /////////////////////////////////////////////////////////////////////
-define(["./module", "./controller1", "./controller2", "./menuController"], function () {
+define(["./module", "./controller1", "./controller2"], function () {
 
 });
