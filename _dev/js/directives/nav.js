9c7025c076faf989ba272cc7436b809dbad9bc30
Lucas Mouilleron <LucasMouileron>
2014-10-16 11:21:34 +0200

    touchfix

--- _dev/js/directives/nav.js
+++ _dev/js/directives/nav.js
@@ -8,7 +8,7 @@ define(["./module", "jquery"], function (directives) {
 
         function link($scope, $element, $attrs) {
 
-            $(".nav a").on("click touchstart", function(){
+            $(".nav a").on("click touchend", function(){
                 $(".btn-navbar").click();
                 $(".navbar-toggle").click()
             });
